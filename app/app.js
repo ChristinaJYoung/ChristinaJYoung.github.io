ea4b471396594b14b6ad0333fc1339777e41a375
ChristinaJYoung <Christina@Billys-MacBook-Pro.local>
2016-04-14 14:39:46 -0500

    adding responsive navigation

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ define(["jquery"], function($) {
     $('#techPage').show('slow');
     $('#homePage').hide();
     $('#bioPage').hide();
+    $('#projectsPage').hide();
+    $('#contactPage').hide();
     console.log("Hello World! ");
   });
 
@@ -11,6 +13,8 @@ define(["jquery"], function($) {
     $('#homePage').show('slow');
     $('#techPage').hide();
     $('#bioPage').hide();
+    $('#projectsPage').hide();
+    $('#contactPage').hide();
     console.log("Hello World! ");
   });
 
@@ -18,7 +22,50 @@ define(["jquery"], function($) {
     $('#bioPage').show('slow');
     $('#homePage').hide();
     $('#techPage').hide();
+    $('#projectsPage').hide();
+    $('#contactPage').hide();
     console.log("Hello World! ");
+  });
+
+  $('#projects').click(function () {
+    $('#projectsPage').show('slow');
+    $('#contactPage').hide();
+    $('#homePage').hide();
+    $('#bioPage').hide();
+    $('#techPage').hide();
+    console.log("Hello World! ");
+  });
+
+  $('#contact').click(function () {
+    $('#contactPage').show('slow');
+    $('#projectsPage').hide();
+    $('#homePage').hide();
+    $('#bioPage').hide();
+    $('#techPage').hide();
+    console.log("Hello World! ");
+  });
+
+// Create the dropdown base
+  $("#dropNav").appendTo("nav");
+
+  // Create default option "Go to..."
+  $("<option />", {
+     "selected": "selected",
+     "value"   : "",
+     "text"    : "Go to..."
+  }).appendTo("nav select");
+
+  // Populate dropdown with menu items
+  $("nav a").each(function() {
+    var el = $(this);
+    $("<option />", {
+       "value"   : el.attr("href"),
+       "text"    : el.text()
+     }).appendTo("nav select");
+  });
+
+  $("nav select").change(function() {
+    window.location = $(this).find("option:selected").val();
   });
 
 });
